ad1e286cb0d91f270d1cfb9dc73406516c5e41f1
chiragmadan4 <chiragmadan4@gmail.com>
2019-07-31 11:39:32 +0530

    Added functionality for 'Checks'

--- src/chess/Game.java
+++ src/chess/Game.java
@@ -1,5 +1,6 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 import pieces.*;
@@ -7,8 +8,8 @@ import pieces.*;
 public class Game {
 	static Scanner scan;
 	static Board board;
-	static Player player1;
-	static Player player2;
+	static Player player1; // white
+	static Player player2; // black
 	static Color turn;
 
 	public static void main(String[] args) {
@@ -23,10 +24,10 @@ public class Game {
 
 	private static void startGame() {
 		turn = Color.WHITE;
-		
+
 		while (true) {
-			System.out.println(turn+"'s turn");
-			
+			System.out.println(turn + "'s turn");
+
 			if (inputMove()) {
 				printBoard();
 				if (turn == Color.WHITE) {
@@ -34,6 +35,9 @@ public class Game {
 				} else {
 					turn = Color.WHITE;
 				}
+				Boolean isUnderCheck = checkForCheck();
+				if (isUnderCheck)
+					System.out.println("CHECK!");
 
 			} else {
 				System.out.println("invalid move, try again");
@@ -42,12 +46,31 @@ public class Game {
 		}
 	}
 
-	private static void initializeGame(Board board) {
-		player1 = new Player(Color.WHITE);
-		player2 = new Player(Color.BLACK);
+	private static boolean checkForCheck() {
+		if (turn == Color.WHITE) { // if any of black pieces(player2) can reach white king
+			return checkForCheck(player2.playersPieces, player1.kingSpot);
+		} else { // if any of white pieces can reach black king
+			return checkForCheck(player1.playersPieces, player2.kingSpot);
+		}
 
-		initializeBlackPieces(board);
-		initializeWhitePieces(board);
+	}
+
+	private static boolean checkForCheck(ArrayList<Piece> attackers, Spot kingSpot) {
+		for (Piece p : attackers) {
+			if (p.isValidMove(board, kingSpot)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static void initializeGame(Board board) {
+		player1 = new Player(Color.WHITE, board.getSpot(7, 4));
+		player2 = new Player(Color.BLACK, board.getSpot(0, 4));
+
+		initializeWhitePieces(board, player1.playersPieces);
+		initializeBlackPieces(board, player2.playersPieces);
+
 	}
 
 	public static boolean inputMove() {
@@ -72,7 +95,18 @@ public class Game {
 
 		if (currPiece.isValidMove(board, dest)) {
 			System.out.println();
+
 			currPiece.makeMove(board, curr, dest);
+
+			if (currPiece.type.equals("K") || currPiece.type.equals("k")) {
+
+				if (turn == Color.WHITE) {
+					player1.kingSpot = dest;
+				} else {
+					player2.kingSpot = dest;
+				}
+
+			}
 			return true;
 		}
 
@@ -111,81 +145,99 @@ public class Game {
 		System.out.println();
 	}
 
-	private static void initializeBlackPieces(Board board) {
+	private static void initializeBlackPieces(Board board, ArrayList<Piece> playersPieces) {
 
 		// initialize pawns
 		for (int i = 0; i < Board.BOARD_SIZE; i++) {
 			Pawn p = new Pawn(Color.BLACK, board.getSpot(1, i));
 			board.getSpot(1, i).setPiece(p);
+			playersPieces.add(p);
 		}
 
 		// initialize rook
 		Rook r1 = new Rook(Color.BLACK, board.getSpot(0, 0));
 		board.getSpot(0, 0).setPiece(r1);
+		playersPieces.add(r1);
 
 		Rook r2 = new Rook(Color.BLACK, board.getSpot(0, 7));
 		board.getSpot(0, 7).setPiece(r2);
+		playersPieces.add(r2);
 
 		// initialize knight
-		Knight k1 = new Knight(Color.BLACK, board.getSpot(0, 1));
-		board.getSpot(0, 1).setPiece(k1);
+		Knight n1 = new Knight(Color.BLACK, board.getSpot(0, 1));
+		board.getSpot(0, 1).setPiece(n1);
+		playersPieces.add(n1);
 
-		Knight k2 = new Knight(Color.BLACK, board.getSpot(0, 6));
-		board.getSpot(0, 6).setPiece(k2);
+		Knight n2 = new Knight(Color.BLACK, board.getSpot(0, 6));
+		board.getSpot(0, 6).setPiece(n2);
+		playersPieces.add(n2);
 
 		// initialize bishop
 		Bishop b1 = new Bishop(Color.BLACK, board.getSpot(0, 2));
 		board.getSpot(0, 2).setPiece(b1);
+		playersPieces.add(b1);
 
 		Bishop b2 = new Bishop(Color.BLACK, board.getSpot(0, 5));
 		board.getSpot(0, 5).setPiece(b2);
+		playersPieces.add(b2);
 
 		// initialize queen
 		Queen q = new Queen(Color.BLACK, board.getSpot(0, 3));
 		board.getSpot(0, 3).setPiece(q);
+		playersPieces.add(q);
 
 		// initialize king
 		King k = new King(Color.BLACK, board.getSpot(0, 4));
 		board.getSpot(0, 4).setPiece(k);
+		playersPieces.add(k);
 
 	}
 
-	private static void initializeWhitePieces(Board board) {
+	private static void initializeWhitePieces(Board board, ArrayList<Piece> playersPieces) {
 
 		// initialize pawns
 		for (int i = 0; i < Board.BOARD_SIZE; i++) {
 			Pawn p = new Pawn(Color.WHITE, board.getSpot(6, i));
 			board.getSpot(6, i).setPiece(p);
+			playersPieces.add(p);
 		}
 
 		// initialize rook
 		Rook r1 = new Rook(Color.WHITE, board.getSpot(7, 0));
 		board.getSpot(7, 0).setPiece(r1);
+		playersPieces.add(r1);
 
 		Rook r2 = new Rook(Color.WHITE, board.getSpot(7, 7));
 		board.getSpot(7, 7).setPiece(r2);
+		playersPieces.add(r2);
 
 		// initialize knight
-		Knight k1 = new Knight(Color.WHITE, board.getSpot(7, 1));
-		board.getSpot(7, 1).setPiece(k1);
+		Knight n1 = new Knight(Color.WHITE, board.getSpot(7, 1));
+		board.getSpot(7, 1).setPiece(n1);
+		playersPieces.add(n1);
 
-		Knight k2 = new Knight(Color.WHITE, board.getSpot(7, 6));
-		board.getSpot(7, 6).setPiece(k2);
+		Knight n2 = new Knight(Color.WHITE, board.getSpot(7, 6));
+		board.getSpot(7, 6).setPiece(n2);
+		playersPieces.add(n2);
 
 		// initialize bishop
 		Bishop b1 = new Bishop(Color.WHITE, board.getSpot(7, 2));
 		board.getSpot(7, 2).setPiece(b1);
+		playersPieces.add(b1);
 
 		Bishop b2 = new Bishop(Color.WHITE, board.getSpot(7, 5));
 		board.getSpot(7, 5).setPiece(b2);
+		playersPieces.add(b2);
 
 		// initialize queen
 		Queen q = new Queen(Color.WHITE, board.getSpot(7, 3));
 		board.getSpot(7, 3).setPiece(q);
+		playersPieces.add(q);
 
 		// initialize king
 		King k = new King(Color.WHITE, board.getSpot(7, 4));
 		board.getSpot(7, 4).setPiece(k);
+		playersPieces.add(k);
 
 	}
 
--- src/chess/Player.java
+++ src/chess/Player.java
@@ -1,14 +1,23 @@
 package chess;
 
+import java.util.ArrayList;
+
+import pieces.Piece;
+
 public class Player {
 	String name;
 	Color color;
-	
-	public Player(Color c) {
+	Spot kingSpot;
+	ArrayList<Piece> playersPieces;
+
+	public Player(Color c, Spot kingSpot) {
+		playersPieces = new ArrayList<>();
 		this.color = c;
+		this.kingSpot = kingSpot;
 	}
-	
+
 	public Player(String name, Color c) {
+		playersPieces = new ArrayList<>();
 		this.name = name;
 		this.color = c;
 	}
